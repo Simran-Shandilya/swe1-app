6ac3ec1e3ff6ca94d78dbedfcee5c6a979193d0c
Simran-Shandilya <simran.shandilya12@gmail.com>
2021-10-24 11:45:28 -0400

    black and flake added

--- polls/admin.py
+++ polls/admin.py
@@ -5,10 +5,3 @@ from django.contrib import admin
 from .models import Question
 
 admin.site.register(Question)
-
-
-
-
-
-
-
--- polls/urls.py
+++ polls/urls.py
@@ -1,4 +1,3 @@
-
 from django.urls import path
 from . import views
 
@@ -15,8 +14,6 @@ from . import views
 
 #     # path('<int:question_id>/vote/', views.vote, name='vote'),
 # ]
-
-
 
 
 # app_name = 'polls'
@@ -40,10 +37,10 @@ from . import views
 # ]
 
 
-app_name = 'polls'
+app_name = "polls"
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
-    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
-    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
+    path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
